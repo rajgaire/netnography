e68e90b0929cad1671d608710104016a4467edfb
Gaire, Raj (Data61, Acton) <Raj.Gaire@csiro.au>
2022-01-04 21:51:17 +1100

    test all three types of reviews

--- sources/tests/test_tripadvisor.py
+++ sources/tests/test_tripadvisor.py
@@ -9,9 +9,12 @@ def main():
     # place = 'Patan (Lalitpur)'
     # country = 'Nepal'
     collector = ReviewCollector("../tripadvisor/chromedriver_win32/chromedriver.exe", False, True)
+    # Test Attractions
+    test_url = "https://www.tripadvisor.com//Attraction_Review-g315764-d310829-Reviews-Patan_Museum-Patan_Lalitpur_Kathmandu_Valley_Bagmati_Zone_Central_Region.html"
+    collector.fetch_attraction_reviews(test_url, "Attraction", f"./temp_attraction.txt")
     # Test Hotel
-    # test_url = "https://www.tripadvisor.com//Hotel_Review-g315764-d6995667-Reviews-The_Inn_Patan-Patan_Lalitpur_Kathmandu_Valley_Bagmati_Zone_Central_Region.html"
-    # collector.fetch_hotel_reviews(test_url, "Hotel", f"./temp_hotel.txt")
+    test_url = "https://www.tripadvisor.com//Hotel_Review-g315764-d6995667-Reviews-The_Inn_Patan-Patan_Lalitpur_Kathmandu_Valley_Bagmati_Zone_Central_Region.html"
+    collector.fetch_hotel_reviews(test_url, "Hotel", f"./temp_hotel.txt")
     # Test Restaurant
     test_url = "https://www.tripadvisor.com//Restaurant_Review-g315764-d1156668-Reviews-Patan_Museum_Cafe-Patan_Lalitpur_Kathmandu_Valley_Bagmati_Zone_Central_Region.html"
     collector.fetch_restaurant_reviews(test_url, "Restaurant", f"./temp_restaurant.txt")
